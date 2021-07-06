e14c01efbdd97b24075fa92783b9edf799559420
Luqman Hassan <reallookmanlook@gmail.com>
2021-07-05 21:34:29 -0700

    Updated JS

--- myscript.js
+++ myscript.js
@@ -7,21 +7,42 @@
 4 - Array gets checked - If Sucess end game - if not continue 
 5 - restart button clears boxes and array
 6 - After GameOver, board cannot be selected & only restart works
-
-
 */
+
 let board = document.getElementById("Board");
 let box = board.getElementsByTagName("div");
+let playerX, playerO;
+let currentPlayer;
+
+currentPlayer = "X";
+document.getElementById("currentPlayer").innerHTML = "Current Player: " + currentPlayer;
+
+
 
 for (let i = 0; i < 9; i++) {
-    box[i].addEventListener("click", fillBox);
+    box[i].addEventListener("click", squares);
 }
 
-function fillBox() {
-    this.innerHTML = "X";
-    this.style.fontSize = "30px";
+function squares() {
+
+    if (this.innerHTML != "X" && this.innerHTML != "O") {
+        this.innerHTML = currentPlayer;
+        this.style.fontSize = "30px";
+        changeplayer();
+    }
 }
 
+function changeplayer() {
+
+    if (currentPlayer == "X") {
+        currentPlayer = "O";
+    } else if (currentPlayer == "O") {
+        currentPlayer = "X";
+    }
+    document.getElementById("currentPlayer").innerHTML = "Current Player: " + currentPlayer;
+
+
+}
 
 function restart() {
     for (let i = 0; i < 9; i++) {
